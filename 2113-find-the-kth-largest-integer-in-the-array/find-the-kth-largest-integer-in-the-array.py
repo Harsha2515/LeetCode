83bd5298ed2b3377a7cace8528b23729e612a4af
Harsha Vardhan Vempadapu <127225488+Harsha2515@users.noreply.github.com>
2026-09-09 22:26:41 +0530

    Time: 17 ms (89.38%) | Memory: 25.9 MB (98.06%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.py
@@ -1,11 +1,4 @@
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        if len(nums)<k:
-            return None
-
-        arr = []
-
-        for i in nums:
-            arr.append(int(i))
-        arr.sort()
-        return str(arr[-k])
+        nums.sort(key = int)
+        return nums[-k]
